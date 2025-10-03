cc4a8d47e06a6a38112606a6ad2bd19a2e181cc5
Mauricio Conto <Mauricio.Conto@corecard.bog>
2025-10-03 10:55:30 -0500

    my first commit

---
+++ src/main/java/com/corecard/file_level/FileLevelDeTokenizationApplication.java
@@ -0,0 +1,13 @@
+package com.corecard.file_level;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileLevelDeTokenizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileLevelDeTokenizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corecard/file_level/FileLevelDeTokenizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corecard.file_level;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileLevelDeTokenizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
